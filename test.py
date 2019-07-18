80cda57bc204a02dbf970bb93273840518a6c0d0
Mark Goldstein <markgoldstein12@gmail.com>
2019-07-17 22:01:43 -0400

    added test file

---
+++ test.py
@@ -0,0 +1,25 @@
+# Full URL Call https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ
+
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__, template_folder='.')
+
+
+@app.route('/')
+def homepage():
+    params = {
+        'api_key': 'vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ',
+    }
+    r = requests.get(
+        'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ&date=2018-11-9')
+    # date = json.loads(r["date"])
+    # Get the dict representation of the response
+    res = r.json()
+
+    return render_template('home.html', img=res['img'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
