e42c017c5fd4ae1f9a5ac75485a405f16d9dbf0f
maker-mark <markgoldstein12@gmail.com>
2019-07-18 12:52:59 -0400

    you can now choose a date to get the POD from!

--- test.py
+++ test.py
@@ -1,20 +1,30 @@
 # Full URL Call https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 
 app = Flask(__name__, template_folder='.')
 
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    print(processed_text)
+    r = requests.get(
+        'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ&date='+processed_text)
+    # Get the dict representation of the response
+    res = r.json()
+    return render_template('home.html', img=res['url'], title=res['title'], desc=res['explanation'])
+
 @app.route('/')
 def homepage():
     params = {
-        'api_key': 'vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ',
+        'api_key': 'vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ'
     }
     r = requests.get(
         'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ')
-    # date = json.loads(r["date"])
     # Get the dict representation of the response
     res = r.json()
 
