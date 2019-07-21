f17d247f5dc8ef71130e33c43b0d59e289ebc5c7
Mark <markgoldstein12@gmail.com>
2019-07-21 14:52:11 -0400

    Added styling and conditionals for rendering a video or a picture

--- test.py
+++ test.py
@@ -9,27 +9,32 @@ app = Flask(__name__, template_folder='.')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    print(processed_text)
+    text = request.form['text'] #flask will send us a request obj, adn from the form we can fish out the text
     r = requests.get(
-        'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ&date='+processed_text)
+        'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ&date='+ text)
     # Get the dict representation of the response
-    res = r.json()
-    return render_template('home.html', img=res['url'], title=res['title'], desc=res['explanation'])
+    res = r.json() # Decodes the response json to python dict
+    isImg = False
+    #If the date is an image
+    if(res['media_type'] == 'image'):
+        isImg = True
+    print(isImg)     
+    return render_template('home.html', url=res['url'], title=res['title'], desc=res['explanation'], type=res['media_type'], isImg=isImg)
 
 @app.route('/')
-def homepage():
+def  homepage():
     params = {
         'api_key': 'vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ'
     }
+    isCool = "kinda"
     r = requests.get(
         'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ')
     # Get the dict representation of the response
     res = r.json()
-
-    return render_template('home.html', img=res['url'], title=res['title'], desc=res['explanation'])
+    if(res['media_type'] == 'image'):
+        isImg = True
+    return render_template('home.html', url=res['url'], title=res['title'], desc=res['explanation'], isImg=isImg)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run( debug=True)
