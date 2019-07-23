cdef72b089ec869ea6d967f729e640da1af45d98
Mark <markgoldstein12@gmail.com>
2019-07-23 15:28:57 -0400

    Added ReadMe

--- app/app.py
+++ app/app.py
@@ -7,29 +7,33 @@ import json
 app = Flask(__name__, template_folder='.')
 
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text'] #flask will send us a request obj, adn from the form we can fish out the text
-    r = requests.get(
-        'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ&date='+ text)
-    # Get the dict representation of the response
-    res = r.json() # Decodes the response json to python dict
-    isImg = False
-    #If the date is an image
-    if(res['media_type'] == 'image'):
-        isImg = True
-    return render_template("/templates/home.html", url=res['url'], title=res['title'], desc=res['explanation'], type=res['media_type'], isImg=isImg)
-
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def  homepage():
+    #If the method is POST
+    if(request.method ==  'POST'):
+        text = request.form['text'] #flask will send us a request obj, adn from the form we can fish out the text
+        r = requests.get(
+            'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ&date='
+            + text)
+        # Get the dict representation of the response
+        res = r.json() # Decodes the response json to python dict
+        isImg = False
+        #If the date is an image
+        if(res['media_type'] == 'image'):
+            isImg = True
+        return render_template("/templates/home.html", url=res['url'], title=res['title'],
+         desc=res['explanation'], type=res['media_type'], isImg=isImg)
 
+    #Otherwise it is a GET request
     r = requests.get(
         'https://api.nasa.gov/planetary/apod?api_key=vZviPQHrjc3jDCPFQNvbWka45EEPIrRAgbepKmvZ')
     # Get the dict representation of the response
     res = r.json()
+    print(type(res))
     if(res['media_type'] == 'image'):
         isImg = True
-    return render_template("/templates/home.html", url=res['url'], title=res['title'], desc=res['explanation'], isImg=isImg)
+    return render_template("/templates/home.html", url=res['url'], 
+    title=res['title'], desc=res['explanation'], isImg=isImg)
 
 
 if __name__ == '__main__':
